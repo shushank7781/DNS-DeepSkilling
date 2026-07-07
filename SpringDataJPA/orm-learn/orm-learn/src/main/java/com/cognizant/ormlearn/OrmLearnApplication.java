2c8282a383cea6b970cb9761bafb0a3238d9602c
shushank7781 <shank232004@gmail.com>
2026-07-07 21:32:00 +0530

    Implemented Spring Data JPA Quick Example

---
+++ SpringDataJPA/orm-learn/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,32 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("===== All Countries =====");
+        countryService.getAllCountries().forEach(System.out::println);
+
+        System.out.println("\n===== Get Country =====");
+        Country country = countryService.getCountry("IN");
+        System.out.println(country);
+
+    }
+}
---
+++ SpringDataJPA/orm-learn/orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,36 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    public Country getCountry(String code) {
+        return countryRepository.findById(code).orElse(null);
+    }
+
+    public Country addCountry(Country country) {
+        return countryRepository.save(country);
+    }
+
+    public Country updateCountry(Country country) {
+        return countryRepository.save(country);
+    }
+
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+}
