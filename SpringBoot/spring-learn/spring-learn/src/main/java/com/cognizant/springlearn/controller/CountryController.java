b65c68abe05c3f05ee4c9626a3ab3afd6384c235
shushank7781 <shank232004@gmail.com>
2026-07-17 21:07:59 +0530

    Completed REST Get Country Based on Country Code exercise

--- SpringBoot/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ SpringBoot/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -2,18 +2,33 @@ package com.cognizant.springlearn.controller;
 
 import com.cognizant.springlearn.Country;
 import com.cognizant.springlearn.service.CountryService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CountryController {
 
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
     @Autowired
     private CountryService countryService;
 
-    @GetMapping("/country")
-    public Country getCountry() {
-        return countryService.getCountry();
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        LOGGER.info("START");
+
+        Country country = countryService.getCountry(code);
+
+        LOGGER.info("END");
+
+        return country;
     }
 }
--- SpringBoot/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
+++ SpringBoot/spring-learn/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -1,18 +1,34 @@
 package com.cognizant.springlearn.service;
 
-import com.cognizant.springlearn.Country;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
+import com.cognizant.springlearn.Country;
+
 @Service
 public class CountryService {
 
-    public Country getCountry() {
+    @SuppressWarnings("unchecked")
+    public Country getCountry(String code) {
 
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("country.xml");
 
-        return context.getBean("country", Country.class);
+        List<Country> countryList =
+                (List<Country>) context.getBean("countryList");
+
+        for (Country country : countryList) {
+
+            if (country.getCode().equalsIgnoreCase(code)) {
+                return country;
+            }
+
+        }
+
+        return null;
     }
+
 }
